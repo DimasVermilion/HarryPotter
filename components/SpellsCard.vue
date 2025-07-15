95fe0159f95a8ccfbdbcbbb8d0c9c33f098ba393
dimasVermilion <dimasduaran@gmail.com>
2025-07-15 08:08:40 +0700

    Add Houses & chars

---
+++ components/SpellsCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card text-center">
+        <p class="font-bold text-black m-4">Spell Name: {{ spells.spell }}</p>
+        <p class="font-bold text-black m-4">Efek : {{ spells.use }}</p>
+
+        
+    </div>
+</template>
+
+<script setup>
+const { spells } = defineProps(['spells'])
+
+</script>
+
+<style scoped>
+.foto {
+    max-height: 120px;
+    max-width: 70%;
+    margin: 0 auto;
+  }
+
+</style>
